d0d3cc0e6bae08b981d2019c7d6e25e03c9093a8
qetiproject <ketideveloper@gmail.com>
2025-04-02 20:36:46 +0400

    createEmbeddedView

--- src/app/widgets/weather-widget/weather-widget.component.ts
+++ src/app/widgets/weather-widget/weather-widget.component.ts
@@ -1,5 +1,5 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, inject, Injector, Input, TemplateRef } from '@angular/core';
+import { Component, inject, Injector, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { WidgetActions } from '../widget-actions.service';
 import { WidgetState } from '../widget-state.service';
 
@@ -14,4 +14,11 @@ import { WidgetState } from '../widget-state.service';
 export class WeatherWidgetComponent {
   state = inject(WidgetState);
   actions = inject(WidgetActions);
+
+  @ViewChild('container', { read: ViewContainerRef}) container!: ViewContainerRef;
+  @ViewChild('defaultWidgetHeader') headerTemplate!: TemplateRef<any>; 
+
+  ngAfterViewInit(): void {
+   this.container.createEmbeddedView(this.headerTemplate);
+  }
 }
