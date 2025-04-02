729d5efdf2e73e52b681e024bf98fc58183f2d1f
qetiproject <ketideveloper@gmail.com>
2025-04-02 20:59:05 +0400

    headerTemplate

--- src/app/widgets/weather-widget/weather-widget.component.ts
+++ src/app/widgets/weather-widget/weather-widget.component.ts
@@ -1,12 +1,12 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, inject, Injector, Input, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { Component, inject, Input, TemplateRef } from '@angular/core';
 import { WidgetActions } from '../widget-actions.service';
 import { WidgetState } from '../widget-state.service';
 
 @Component({
   selector: 'weather-widget',
   standalone: true,
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './weather-widget.component.html',
   styleUrls: ['./weather-widget.component.css'],
   providers: [WidgetActions, WidgetState]
@@ -15,10 +15,8 @@ export class WeatherWidgetComponent {
   state = inject(WidgetState);
   actions = inject(WidgetActions);
 
-  @ViewChild('container', { read: ViewContainerRef}) container!: ViewContainerRef;
-  @ViewChild('defaultWidgetHeader') headerTemplate!: TemplateRef<any>; 
+  @Input()
+  headerTemplate!: TemplateRef<any>;
 
-  ngAfterViewInit(): void {
-   this.container.createEmbeddedView(this.headerTemplate);
-  }
+  @Input() contentTemplate!: TemplateRef<any>;
 }
