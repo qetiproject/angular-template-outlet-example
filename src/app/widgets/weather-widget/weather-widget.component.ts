7e0c9e231d992f83f72bd9b7c4a1a1e22e9f64c5
qetiproject <ketideveloper@gmail.com>
2025-04-03 11:39:45 +0400

    dynamic actions

--- src/app/widgets/weather-widget/weather-widget.component.ts
+++ src/app/widgets/weather-widget/weather-widget.component.ts
@@ -1,5 +1,5 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, inject, Input, TemplateRef } from '@angular/core';
+import { Component, inject, Injector, Input, TemplateRef } from '@angular/core';
 import { WidgetActions } from '../widget-actions.service';
 import { WidgetState } from '../widget-state.service';
 
@@ -14,8 +14,11 @@ import { WidgetState } from '../widget-state.service';
 export class WeatherWidgetComponent {
   state = inject(WidgetState);
   actions = inject(WidgetActions);
+  injector = inject(Injector)
 
   @Input() headerTemplate!: TemplateRef<any>;
 
   @Input() contentTemplate!: TemplateRef<WidgetState>;
+
+  @Input() actionTemplate!: TemplateRef<any>;
 }
